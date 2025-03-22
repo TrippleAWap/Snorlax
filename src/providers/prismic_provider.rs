313bd8deda2fa638431643bc8000624e357304ca
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2025-03-22 08:45:00 -0600

    Update prismic_provider.rs

--- src/providers/prismic_provider.rs
+++ src/providers/prismic_provider.rs
@@ -1,29 +1,32 @@
-use std::fmt::Debug;
-use rusqlite::{params, Connection, Error};
-use tokio::sync::Mutex;
 use crate::providers::provider::{DatabaseEntry, Provider};
+use rusqlite::{Connection, Error, params};
+use std::fmt::Debug;
+use std::time::Instant;
+use tokio::sync::Mutex;
 
 #[derive(Debug)]
 pub struct PrismicProvider {
     pub(crate) database_connection: Mutex<Connection>,
     pub(crate) table_name: String,
 }
-
-const PRISMIC_DATABASE_URL: &str = "https://gist.githubusercontent.com/Mwr247/ef9a06ee1d3209a558b05561f7332d8e/raw/vrcavtrdb.txt";
+const PRISMIC_DATABASE_URL: &str =
+    "https://gist.githubusercontent.com/Mwr247/ef9a06ee1d3209a558b05561f7332d8e/raw/vrcavtrdb.txt";
 
 async fn get_prismic_database() -> Vec<DatabaseEntry> {
     let mut entries = vec![];
-    let response = reqwest::get(PRISMIC_DATABASE_URL).await.unwrap().text().await.unwrap();
-
+    let response = reqwest::get(PRISMIC_DATABASE_URL)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
     let lines = response.split('\n');
     for line in lines {
         let line = line.chars().rev().collect::<String>();
         let parts: Vec<&str> = line.split('\t').rev().collect();
-        // println!("{:?}", parts.len());
         if parts.len() == 4 {
-            let avatar_id = decode_avatar_id(parts[0].chars().rev().collect::<String>().as_str());
+            let avatar_id = decode_avatar_id(parts[0]);
             if avatar_id.is_err() {
-                // println!("Invalid avatar id {} - {}", parts[0], avatar_id.err().unwrap());
                 continue;
             }
             let avatar_name = parts[1].to_string();
@@ -35,7 +38,6 @@ async fn get_prismic_database() -> Vec<DatabaseEntry> {
                 avatar_description,
                 author_id,
             };
-            println!("{:?}", entry);
             // avtr_a1c59c2f-b11e-48ad-9ba7-d7e0fede57d3
             // avtr_0009c2a9-265d-497a-7ba8-6912bb83ee5c
             entries.push(entry);
@@ -45,57 +47,76 @@ async fn get_prismic_database() -> Vec<DatabaseEntry> {
     entries
 }
 
-const CIPHER: &str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+=";
-
+const CIPHER: &str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+";
 fn decode_avatar_id(crypt: &str) -> Result<String, Box<dyn std::error::Error>> {
     if crypt.len() != 24 {
-        return Err("Invalid input length".into());
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Invalid input length",
+        )));
     }
-
-    let mut decrypt: Vec<u8> = vec![0; 33];
-    let new_format = (CIPHER.find(crypt.chars().nth(21).unwrap()).unwrap() >> 2) & 2;
+    let mut decrypt: Vec<char> = vec![' '; 33];
+    let index_of_char = match CIPHER.find(crypt.chars().nth(21).unwrap()) {
+        Some(v) => v,
+        None => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Invalid input",
+            )));
+        }
+    };
+    let new_format = (index_of_char >> 2) & 2;
 
     for i in 0..11 {
-        let a = crypt.chars().nth(i * 2).unwrap();
-        let b = crypt.chars().nth(i * 2 + 1).unwrap();
-
-        let a_index = CIPHER.find(a).ok_or("Invalid character in input")?;
-        let b_index = CIPHER.find(b).ok_or("Invalid character in input")?;
-
         let idx = i * 3;
-
-        let val1 = ((a_index >> new_format) & 15) as u8;
-        let val2 = if new_format == 0 {
-            (((a_index >> 2) & 12) | ((b_index >> 4) & 3)) as u8
-        } else {
-            ((((a_index & 3) << 2) | ((b_index >> 4) & 3))) as u8
+        let first = match CIPHER.find(crypt.chars().nth(i * 2).unwrap()) {
+            Some(v) => v,
+            None => {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Invalid input",
+                )));
+            }
         };
-        let val3 = (b_index & 15) as u8;
 
-        decrypt[idx] = CIPHER.as_bytes()[val1];
-        decrypt[idx + 1] = CIPHER.as_bytes()[val2];
-        decrypt[idx + 2] = CIPHER.as_bytes()[val3];
+        let third = match CIPHER.find(crypt.chars().nth(i * 2 + 1).unwrap()) {
+            Some(v) => v,
+            None => {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Invalid input",
+                )));
+            }
+        };
+        decrypt[idx] = CIPHER.chars().nth((first >> new_format) & 15).unwrap();
+        let second = if new_format == 0 {
+            (first >> 2) & 12
+        } else {
+            (first & 3) << 2
+        };
+
+        decrypt[idx + 1] = CIPHER.chars().nth(second | ((third >> 4) & 3)).unwrap();
+        decrypt[idx + 2] = CIPHER.chars().nth(third & 15).unwrap();
     }
 
     decrypt.pop();
 
-    let mut uuid = String::new();
-    for (i, &c) in decrypt.iter().enumerate() {
-        if i == 8 || i == 13 || i == 18 || i == 23 {
-            uuid.push('-');
-        }
-        uuid.push(c as char);
-    }
+    decrypt.insert(8, '-');
+    decrypt.insert(13, '-');
+    decrypt.insert(18, '-');
+    decrypt.insert(23, '-');
 
-    Ok(format!("avtr_{}", uuid))
+    let decrypted_str: String = decrypt.into_iter().collect();
+
+    Ok(format!("avtr_{}", decrypted_str))
 }
-
 
 impl Provider for PrismicProvider {
     async fn on_start(&self) -> () {
         let conn = self.database_connection().lock().await;
         conn.execute(
-            &format!("CREATE TABLE IF NOT EXISTS {} (
+            &format!(
+                "CREATE TABLE IF NOT EXISTS {} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
 
             avatar_id TEXT UNIQUE NOT NULL,
@@ -104,10 +125,12 @@ impl Provider for PrismicProvider {
             avatar_description TEXT NOT NULL,
 
             author_id TEXT NOT NULL
-        )", self.table_name),
+        )",
+                self.table_name
+            ),
             params![],
         )
-            .unwrap();
+        .unwrap();
     }
     fn database_connection(&self) -> &Mutex<Connection> {
         &self.database_connection
@@ -131,27 +154,54 @@ impl Provider for PrismicProvider {
         Ok(result)
     }
 
-    async fn add_entries(&self, entries: Vec<DatabaseEntry>) -> Result<(), Error> {
-        let conn = self.database_connection().lock().await;
-        let mut stmt = conn.prepare(&format!("INSERT INTO {} (avatar_id, avatar_name, avatar_description, author_id) VALUES (?,?,?,?)", self.table_name))?;
-        for entry in entries {
-            stmt.execute(
-                params![
-                    entry.avatar_id,
-                    entry.avatar_name,
-                    entry.avatar_description,
-                    entry.author_id,
-                ],
+    async fn add_entries(&self, avatars: Vec<DatabaseEntry>) -> Result<(), Error> {
+        const CHUNK_SIZE: usize = 250; // SQLite's max is 999 params: 250 * 4 = 1000 params
+        let mut conn = self.database_connection().lock().await;
+        let start_time = Instant::now();
+        let tx = conn.transaction()?;
+        {
+            let mut stmt = tx.prepare_cached(
+                "INSERT OR IGNORE INTO avatars
+        (avatar_id, avatar_name, avatar_description, author_id)
+        VALUES (?, ?, ?, ?)",
             )?;
+
+            for (i, chunk) in avatars.chunks(CHUNK_SIZE).enumerate() {
+                for avatar in chunk {
+                    stmt.execute(params![
+                        avatar.avatar_id,
+                        avatar.avatar_name,
+                        avatar.avatar_description,
+                        avatar.author_id,
+                    ])?;
+                }
+
+                if i % 100 == 0 {
+                    let processed = (i + 1) * CHUNK_SIZE;
+                    println!(
+                        "Inserted {} of {} ({:.2}%)",
+                        processed.min(avatars.len()),
+                        avatars.len(),
+                        (processed as f32 / avatars.len() as f32) * 100.0
+                    );
+                }
+            }
         }
+        tx.commit()?;
+        println!(
+            "Inserted {} avatars in {:?}",
+            avatars.len(),
+            start_time.elapsed()
+        );
         Ok(())
     }
 
     async fn spawn_thread(&self) -> () {
-        let db = get_prismic_database().await;
+        println!("Fetching data from Prismic...");
+        let mut db = get_prismic_database().await;
         println!("Adding {} entries to the database...", db.len());
+        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
         self.add_entries(db).await.unwrap();
         println!("Database updated successfully!");
     }
 }
-
